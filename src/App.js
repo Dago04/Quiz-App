eaec0b835418cfc3b93acf2472ae1cec3c654f42
Dagoberto Salas <DagoSalas1999@gmail.com>
2024-04-06 22:37:19 -0600

    Handling loading, Error, and Ready status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loading from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -27,7 +30,8 @@ function reducer(state, action) {
 
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-
+  const { questions, status } = state;
+  const questionsLenght = questions.length;
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -38,8 +42,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loading />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && <StartScreen questionsLenght={questionsLenght} />}
       </Main>
     </div>
   );
